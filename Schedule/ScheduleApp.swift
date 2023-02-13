ec9f0b9f52b3142859b9d551c35b0086b7ce1c08
Nikita Usov <usov107@gmail.com>
2023-02-13 21:55:19 +0700

    Initial Commit

---
+++ Schedule/ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleApp.swift
+//  Schedule
+//
+//  Created by admin on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
