6fb276a5a6056ce6f7bf0e91650cdc2933d72f1f
Nikita Usov <usov107@gmail.com>
2023-03-10 18:12:41 +0700

    Добавлен недобавленный ранее файл

---
+++ Schedule/ClassroomPickerScreenModel.swift
@@ -0,0 +1,15 @@
+//
+//  TeacherPickerScreenModel.swift
+//  Schedule
+//
+//  Created by admin on 24.02.2023.
+//
+
+import Foundation
+
+struct ClassroomPickerScreenModel {
+    
+    var classroomList: [ClassroomModel] = []
+    var classroomText: String = ""
+    
+}
